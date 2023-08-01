feb3692c46f52d70e5a5eba4502516899ee305f7
izharahmad123 <106739395+izharahmad123@users.noreply.github.com>
2023-08-01 16:49:14 +0500

    UI Integration

---
+++ Assets/Scripts/Splash.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Splash : MonoBehaviour
+{
+
+    public GameObject childPrefab; // Reference to the prefab of the child GameObject
+    public Transform parentObject; // Reference to the parent GameObject's Transform component
+     
+    int Value;
+    Scene activeScene;
+    private void Start()
+    {
+        Value = 0;
+        StartCoroutine(waitforSceneSwitch());
+        activeScene = SceneManager.GetActiveScene();
+
+    }
+
+
+    IEnumerator waitforSceneSwitch()
+    {
+        Time.timeScale = 1f;
+        yield return new WaitForSeconds(0.3f);
+        
+        // Instantiate the child GameObject from the prefab
+        GameObject instantiatedChild = Instantiate(childPrefab, childPrefab.transform.position, childPrefab.transform.rotation);
+        
+        // Set the parent of the instantiated child to the desired parent GameObject
+        instantiatedChild.transform.parent = parentObject;
+        instantiatedChild.transform.localScale = new Vector3(1f, 1f, 1f);
+        Value =Value + 1;
+        CheckTime();
+    }
+
+    public void CheckTime()
+    {
+        if (Value >= 71)
+        {
+            StopCoroutine(waitforSceneSwitch());
+            if (activeScene.name == "Splash")
+                SceneManager.LoadScene("MainMenu");
+            else if (activeScene.name == "MainMenu")
+                Debug.Log("SceneCallHere");
+        }
+        else
+        {
+            StartCoroutine(waitforSceneSwitch());
+        }
+    }
+
+}
